fd5f22b03bbd69b6b9eb25bb48b3fd7b14bfea6f
admin <ravii.chunduru@gmail.com>
2024-06-03 13:57:58 +0530

    added GetActiveElement.java

---
+++ src/test/java/GetActiveElement.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GetActiveElement {
+  @Test
+  public void getActiveElement() {
+
+      WebDriver driver = new ChromeDriver();
+      driver.get("http://www.google.com");
+      driver.findElement(By.cssSelector("[name='q']")).sendKeys("webElement");
+
+      // Get attribute of current active element
+      System.out.println("Title of Active Element : " +driver.switchTo().activeElement().getAttribute("title"));
+      driver.quit();
+  }
+}
