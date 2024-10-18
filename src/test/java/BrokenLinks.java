87cf6d4a53aedb438edc4094a10473488a534bee
admin <ravii.chunduru@gmail.com>
2024-10-19 02:44:44 +0530

    added BrokenLinks.java

---
+++ src/test/java/BrokenLinks.java
@@ -0,0 +1,75 @@
+import lombok.SneakyThrows;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class BrokenLinks {
+
+  private WebDriver driver;
+  private List<String> validLinks = new ArrayList<>();
+  private List<String> brokenLinks = new ArrayList<>();
+
+  @BeforeTest
+  void setUp() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+
+    driver = new ChromeDriver(options);
+    driver.manage().window().maximize();
+    driver.get("https://www.amazon.in/");
+  }
+
+  @SneakyThrows
+  @Test
+  void brokenLinks() {
+    List<WebElement> elements = driver.findElements(By.tagName("a"));
+    System.out.println("Total links on the page: " + elements.size());
+
+    elements.parallelStream()
+            .map(ele->ele.getAttribute("href"))
+            .filter(stringURL->Objects.nonNull(stringURL) && !(stringURL.isBlank()) )
+            .forEach(stringURL-> findIfBrokenLink(stringURL));
+
+
+    System.out.println("Valid links count: " + validLinks.size());
+    System.out.println("Broken links count: " + brokenLinks.size());
+  }
+
+  private void findIfBrokenLink(String stringURL) {
+    try {
+        URL url = new URL(stringURL);
+        HttpURLConnection huc = (HttpURLConnection) (url.openConnection());
+        huc.setRequestMethod("HEAD");
+        huc.connect();
+        int responseCode = huc.getResponseCode();
+
+        if (responseCode >= 400 && responseCode < 600) {
+          brokenLinks.add(stringURL);
+        } else {
+          validLinks.add(stringURL);
+        }
+      } catch (Exception e) {
+        /*System.err.println("Error checking URL: " + stringURL);
+        e.printStackTrace();*/
+      }
+    }
+
+  @AfterTest
+  void tearDown() {
+    driver.quit();
+  }
+}
+
